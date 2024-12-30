b0040e6a5b24e0d1e164bc57218d34afe8d95154
Khurshedspi <khurshed5322@gmail.com>
2024-12-30 23:42:08 +0600

    Service Details page part complete

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-    origin:[
-      "http://localhost:5173"
-    ]
-  }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 
 const port = process.env.PORT || 5000;
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fmklx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -23,37 +22,79 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const servicesCollection = client.db('services').collection('service');
+    const servicesCollection = client.db("services").collection("service");
+    const reviewCollection = client.db("services").collection("review");
 
-    app.get('/services', async(req, res) =>{
-        const result = await servicesCollection.find().toArray();
+    app.get("/services", async (req, res) => {
+      const result = await servicesCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/services", async (req, res) => {
+      const services = req.body;
+      const result = await servicesCollection.insertOne(services);
+      res.send(result);
+    });
+
+    app.get("/userReview", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/userReview/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { reviewId: id }; // Filter by reviewId
+      const result = await reviewCollection.find(filter).toArray();
+      res.send(result);
+    });
+
+    app.post("/userReview", async (req, res) => {
+      const services = req.body;
+      const result = await reviewCollection.insertOne(services);
+
+      const query = { _id: new ObjectId(services.reviewId) };
+      const service = await servicesCollection.findOne(query);
+      let updateDoc = {};
+
+      if (service.reviewCount) {
+        updateDoc = {
+          $inc: { reviewCount: 1 },
+        };
+      } else {
+        updateDoc = {
+          $set: { reviewCount: 1 },
+        };
+      }
+      const options = { upsert: true };
+      const updateRes = await servicesCollection.updateOne(
+        query,
+        updateDoc,
+        options
+      );
+      if (updateRes.modifiedCount > 0) {
         res.send(result);
-    })
-
-    app.get('/services/:id', async(req, res) =>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)}
-        const result = await servicesCollection.findOne(query);
-        res.send(result);
-    })
-
-    app.post('/services', async(req, res) => {
-        const services = req.body;
-        const result = await servicesCollection.insertOne(services)
-        res.send(result)
-    })
-
+      }
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -61,12 +102,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("This Movie server is Running Now");
-  });
+  res.send("This Movie server is Running Now");
+});
 
 app.listen(port, () => {
-    console.log(port,'is running')
-})
+  console.log(port, "is running");
+});
